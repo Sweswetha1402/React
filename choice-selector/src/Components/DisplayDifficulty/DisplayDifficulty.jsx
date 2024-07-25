8c4b4bf052f6918116b3d927672a551a35f3e040
Sweswetha1402 <113255627+Sweswetha1402@users.noreply.github.com>
2024-07-25 16:01:03 +0530

    Delete choice-selector directory

--- choice-selector/src/Components/DisplayDifficulty/DisplayDifficulty.jsx
+++
@@ -1,6 +0,0 @@
-import s from "./style.module.css"
-export function DisplayDifficulty(props){
-    return<>
-    <div className={s.container}>Set Difficulty : {props.Difficulty?props.Difficulty:"No difficulty set"}</div>
-    </>
-}
--- choice-selector/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { App } from "./App.js";
-
-
-const rootdiv = document.getElementById("root")
-const reactdom = ReactDOM.createRoot(rootdiv)
-reactdom.render(<App/>)
